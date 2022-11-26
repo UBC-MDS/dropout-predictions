7216744efdcd032c40a18d0e0fd4b73384f500df
Ranjitprakash Sundaramurthi <ranjitprakash1986@gmail.com>
2022-11-25 18:02:58 -0800

    Updated naive bayes script.

--- src/naive_bayes_training.py
+++ src/naive_bayes_training.py
@@ -4,7 +4,7 @@
 """
 A script that train and find the best model and store it under the results/ folder.
 
-Usage: src/training.py --train=<train> --out_dir=<out_dir>
+Usage: src/naive_bayes_training.py --train=<train> --out_dir=<out_dir>
  
 Options:
 --train=<train>       Input path for the train dataset
@@ -29,7 +29,7 @@ from sklearn.model_selection import (
     cross_validate,
     train_test_split,
 )
-from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import BernoulliNB, GaussianNB, MultinomialNB
 from sklearn.pipeline import Pipeline, make_pipeline
 
 # logistic regression and random forest
@@ -42,52 +42,42 @@ def main(train, out_dir):
     train_df = pd.read_csv(train)
     X_train, y_train = train_df.drop(columns=["Target"]), train_df["Target"]
 
-    log_reg = LogisticRegression(random_state=123, max_iter=1000)
-    # hyper-parameter tuning for two models
-    param_dist_log = {"class_weight": [None, "balanced"], "C": loguniform(1e-3, 1e3)}
-
-    # output the best model to local
-    random_search_log = hyper_tuning(log_reg, param_dist_log)
-    random_search_log.fit(X_train, y_train)
-    final_model_log = random_search_log
-    cross_val_results = output_object(
-        final_model_log, cross_val_results, X_train, y_train
-    )
-
+    NB = GaussianNB()
+    
+    # output the model to local
+    # results_dict["NB_f1"] = mean_std_cross_val_scores(
+    #     make_pipeline(NB),
+    #     X_train,
+    #     y_train,
+    #     cv = 10,
+    #     scoring = 'f1',
+    #     return_train_score = True,
+    #     n_jobs = -1,
+    #     verbose = 1
+    # )
+    
+    # fit the model
+    NB.fit(X_train, y_train)
+    final_model_log = NB
+    
+    cross_val_results = output_object(NB, cross_val_results, X_train, y_train)
+    
     # save file
     try:
-        file_log = open(out_dir + "/model_try", "wb")
+        file_log = open(out_dir + "/model_try_Naive_Bayes", "wb")
     except:
         os.makedirs(os.path.dirname(out_dir))
-        file_log = open(out_dir + "/model_try", "wb")
+        file_log = open(out_dir + "/model_try_Naive_Bayes", "wb")
 
     pickle.dump(final_model_log, file_log)
 
-    file_model_result = open(out_dir + "/model_result_try", "wb")
+    file_model_result = open(out_dir + "/model_try_Naive_Bayes", "wb")
     pickle.dump(cross_val_results, file_model_result)
-
-
-def hyper_tuning(model, param_dist):
-    scoring_metrics = "f1"
-
-    # output the best model to local
-    random_search = RandomizedSearchCV(
-        model,
-        param_dist,
-        n_iter=10,
-        n_jobs=-1,
-        verbose=1,
-        random_state=123,
-        return_train_score=True,
-        scoring=scoring_metrics,
-    )
-
-    return random_search
 
 
 def output_object(best_model, result_dict, train_x, train_y):
     scoring_metrics = "f1"
-    result_dict["logreg"] = (
+    result_dict["NB_f1"] = (
         pd.DataFrame(
             cross_validate(
                 best_model,
