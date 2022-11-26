18a2d15d4b26c911339b9c5fee11411b9ab99f29
Caesar Wong <caesar.wongw@gmail.com>
2022-11-26 09:42:07 -0800

    correct sh path

--- src/data_analysis_pipeline.sh
+++ src/data_analysis_pipeline.sh
@@ -13,7 +13,7 @@ python download_data.py --url="https://archive-beta.ics.uci.edu/static/ml/datase
 python preprocessing.py --input_path="../data/raw/data.csv" --sep=';' --test_size=0.2 --random_state=522 --output_path="../data/processed"
 
 # generate EDA plot
-python general_EDA.py --input_path="../data/processed" --output_path="../result/eda/"
+python general_EDA.py --input_path="../data/processed/train_eda.csv" --output_path="../results/"
 
 # model training
 python training.py --train="../data/processed/train.csv" --scoring_metrics="f1" --out_dir="../results/"
