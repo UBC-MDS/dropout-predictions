56fa6ea42a98d3fdbccb0256cf4445a5bc297d17
Caesar Wong <caesar.wongw@gmail.com>
2022-11-26 09:40:19 -0800

    adding print msg util function

---
+++ src/utils/print_msg.py
@@ -0,0 +1,14 @@
+# Author: Caesar Wong
+# Date: 2022-11-26
+
+"""
+This script is for printing messages
+Options:
+<None>
+"""
+
+
+def print_msg(msg):
+    print("="*60)
+    print("\t"+msg)
+    print("="*60)
